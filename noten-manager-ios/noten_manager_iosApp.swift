637ae0b7f2f94ec62b70f9665a9dd882ce8ae5e2
ChristophTabTeam <clabestin@icloud.com>
2025-11-18 04:39:59 +0100

    Initial Commit

---
+++ noten-manager-ios/noten_manager_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  noten_manager_iosApp.swift
+//  noten-manager-ios
+//
+//  Created by Christoph Labestin on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct noten_manager_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
